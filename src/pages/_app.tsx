817786ac85774b3743e292ab791efc1cebbabdbe
joao-vi <joaovictor.go.ro@gmail.com>
2022-03-18 08:27:15 -0400

    feat: global style config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyles } from "../theme/global";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
