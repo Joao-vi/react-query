285fea4541e42ceac17f50d96583bd0b4418cfc1
joao-vi <joaovictor.go.ro@gmail.com>
2022-03-28 11:05:29 -0300

    feat: add input component

--- src/components/elements/input/index.tsx
+++ src/components/elements/input/index.tsx
@@ -1,5 +1,21 @@
+import { InputHTMLAttributes } from "react";
 import * as S from "./styles";
 
-export const Input = () => {
-  return <h1>input</h1>;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  id: string;
+  value: string;
+  onChange: () => void;
+  placeHolder: string;
+}
+export const Input = (props: InputProps) => {
+  const { id, value, onChange, placeHolder, ...rest } = props;
+  return (
+    <S.Wrapper
+      id={id}
+      value={value}
+      onChange={onChange}
+      placeholder={placeHolder}
+      {...rest}
+    />
+  );
 };
